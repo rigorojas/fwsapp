2b446b5b72530d183fcb011eebf2a04a94aafa62
Rigo Rojas <rigo.rojas@nfl.com>
2017-01-13 16:35:43 -0800

    feature: added external image sources

--- src/Scenes/Temperature/Temperature.js
+++ src/Scenes/Temperature/Temperature.js
@@ -1,50 +1,68 @@
 import React, { Component } from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import MapView from 'react-native-maps';
 import Api from "../../Components/OpenWeatherMap/Api";
 
 export default class Temperature extends Component {
-    state = {
-        pin: {
-          latitude: 0,
-          longitude: 0,
-        },
-        city: '',
-        temperature: '',
-        description: '',
-     };
-
-    render() {
-        const {openDrawerMenu} = this.props;
+//     state = {
+//         pin: {
+//           latitude: 0,
+//           longitude: 0,
+//         },
+//         city: '',
+//         temperature: '',
+//         description: '',
+//      };
+//
+//     render() {
+//         const {openDrawerMenu} = this.props;
+//         return (
+//             <View style={styles.container}>
+//                 <MapView
+//                     annotations={[this.state.pin]}
+//                     onRegionChangeComplete={this.onRegionChangeComplete}
+//                     style={styles.map}
+//                 >
+//                 </MapView>
+//                 <View style={styles.textWrapper}>
+//                     <Text style={styles.text}>{this.state.city}</Text>
+//                     <Text style={styles.text}>{this.state.temperature}</Text>
+//                     <Text style={styles.text} >{this.state.description}</Text>
+//                 </View>
+//             </View>
+//         );
+//     }
+//
+//     onRegionChangeComplete = (region) => {
+//         this.setState({
+//             pin: {
+//                 latitude: region.latitude,
+//                 longitude: region.longitude,
+//             }
+//         });
+//         Api(region.latitude, region.longitude)
+//         .then((data) => {
+//             this.setState(data);
+//         });
+//     };
+    render(){
         return (
-            <View style={styles.container}>
-                <MapView
-                    annotations={[this.state.pin]}
-                    onRegionChangeComplete={this.onRegionChangeComplete}
-                    style={styles.map}
-                >
-                </MapView>
-                <View style={styles.textWrapper}>
-                    <Text style={styles.text}>{this.state.city}</Text>
-                    <Text style={styles.text}>{this.state.temperature}</Text>
-                    <Text style={styles.text} >{this.state.description}</Text>
-                </View>
+            <View>
+                <Image
+                    style={{width: 300, height: 300}}
+                    source={{uri: 'https://s-media-cache-ak0.pinimg.com/564x/d2/5e/89/d25e89e3a5bdaff978644ed0ff7ba8fa.jpg'}}
+                />
+                <Image
+                    style={{width: 300, height: 300}}
+                    source={{uri: 'https://scontent.xx.fbcdn.net/v/t1.0-9/15894732_10154307555562984_5798618726443862802_n.jpg?oh=3abf9d3825c0f561099ebd2be0c8ac23&oe=58DBE644'}}
+                />
+                <Image
+                    style={{width: 300, height: 300}}
+                    source={{uri: 'http://www.techno4.us/custom/plugin/page/content/main/img/bg_bodyDesktop4.jpg'}}
+                />
             </View>
         );
     }
-
-    onRegionChangeComplete = (region) => {
-        this.setState({
-            pin: {
-                latitude: region.latitude,
-                longitude: region.longitude,
-            }
-        });
-        Api(region.latitude, region.longitude)
-        .then((data) => {
-            this.setState(data);
-        });
-    };
 }
 
 const styles = StyleSheet.create({
