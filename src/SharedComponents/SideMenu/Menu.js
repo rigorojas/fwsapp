49784aa3b8cb32f46f85348459fb93726444da98
Rigo Rojas <rigo.rojas@nfl.com>
2017-01-23 17:35:56 -0800

    added the background image in the drawer

--- src/SharedComponents/SideMenu/Menu.js
+++ src/SharedComponents/SideMenu/Menu.js
@@ -1,50 +1,59 @@
 import React from 'react';
-import {ScrollView, StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {ScrollView, StyleSheet, Text, Image,TouchableOpacity} from 'react-native';
 import {Actions} from 'react-native-router-flux';
 
 export class Menu extends React.Component {
     render() {
         return (
-            <ScrollView style={styles.container}>
-                <Text style={styles.controlText}>
-                    Please Select a Project
-                </Text>
-                <TouchableOpacity
-                    onPress={() => {
-                        setTimeout(
-                            () => Actions.Temperature(),
-                            0
-                        );
-                        Actions.refresh({
-                            key: 'Drawer',
-                            open: false
-                        });
-                    }}
-                    style={styles.button}
-                >
-                    <Text>Temperature</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={() => {
-                        Actions.refresh({
-                            key: 'Drawer',
-                            open: false
-                        })
-                    }}
-                    style={styles.button}
-                >
-                    <Text>Close Drawer</Text>
-                </TouchableOpacity>
-            </ScrollView>
+            <Image
+                style={styles.container}
+                source={{uri: 'https://scontent.xx.fbcdn.net/v/t1.0-9/15894732_10154307555562984_5798618726443862802_n.jpg?oh=3abf9d3825c0f561099ebd2be0c8ac23&oe=58DBE644'}}
+            >
+                <ScrollView style={styles.scrollView}>
+                    <Text style={styles.controlText}>
+                        Please Select a Project
+                    </Text>
+                    <TouchableOpacity
+                        onPress={() => {
+                            setTimeout(
+                                () => Actions.Temperature(),
+                                0
+                            );
+                            Actions.refresh({
+                                key: 'Drawer',
+                                open: false
+                            });
+                        }}
+                        style={styles.button}
+                    >
+                        <Text>Temperature</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => {
+                            Actions.refresh({
+                                key: 'Drawer',
+                                open: false
+                            })
+                        }}
+                        style={styles.button}
+                    >
+                        <Text>Close Drawer</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </Image>
         );
     }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: "#999999"
+      flex: 1,
+      resizeMode: 'cover'
+  },
+  scrollView: {
+      flex: 1,
+      padding: 20,
+      backgroundColor: "transparent"
   },
   controlText: {
     color: 'white',
