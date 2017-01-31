f011706945efa612be143f5d8e094ed9326e1f02
Rigo Rojas <rigo.rojas@nfl.com>
2017-01-30 18:16:17 -0800

    Abstracting styles and images from the main logic

--- index.ios.js
+++ index.ios.js
@@ -6,12 +6,24 @@ import {NavBarImage} from './src/SharedComponents/NavBarImage/NavBarImage.js';
 import Blank from "./src/Scenes/Blank/Blank.js";
 import Temperature from "./src/Scenes/Temperature/Temperature.js";
 
+const RightButtonImage = require('./src/Themes/default/images/gear.png');
+
 const reducerCreate = params => {
     const defaultReducer = Reducer(params);
     return (state, action)=>{
         return defaultReducer(state, action);
     }
 };
+
+const styles = StyleSheet.create({
+    navigationBarStyle: {
+        backgroundColor: "#999999",
+    },
+    rightButtonIconStyle: {
+        width: 20,
+        height: 20,
+    }
+});
 
 class fwsapp extends React.Component {
     state = {};
@@ -21,25 +33,27 @@ class fwsapp extends React.Component {
                 <Scene key="Drawer"
                     component={Side}
                     open={false}
+
                 >
-                    <Scene title="Home" key="root">
+                    <Scene
+                        title="Home"
+                        key="root"
+                    >
                         <Scene
                             key="Blank"
                             component={Blank}
                             renderTitle={() => {return <NavBarImage />;}}
                             hideNavBar={false}
                             initial={true}
+                            navigationBarStyle={styles.navigationBarStyle}
                         />
                         <Scene
                             key="Temperature"
                             component={Temperature}
                             title="Temperature"
                             hideNavBar={false}
-                            rightButtonIconStyle={{
-                                width: 20,
-                                height: 20,
-                            }}
-                            rightButtonImage={require('./src/Themes/default/images/gear.png')}
+                            rightButtonIconStyle={styles.rightButtonIconStyle}
+                            rightButtonImage={RightButtonImage}
                             onRight={() => {alert('right button'); }}
                         />
                     </Scene>
