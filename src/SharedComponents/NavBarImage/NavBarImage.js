eaf1f4100814620c1f45d10de30faf0c285584ef
Rigo Rojas <rigo.rojas@nfl.com>
2017-01-31 12:00:25 -0800

    Styles clean up

--- src/SharedComponents/NavBarImage/NavBarImage.js
+++ src/SharedComponents/NavBarImage/NavBarImage.js
@@ -5,13 +5,10 @@ const appLogo = require('./../../Themes/default/images/appLogo.png');
 const styles = StyleSheet.create({
     container: {
         alignItems: 'center',
-        paddingTop: 26,
-        backgroundColor: '#cccc00'
     },
     navBarImage: {
         width: 238,
         height: 25,
-        marginBottom: 150
     }
 });
 
