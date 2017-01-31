931fbd234db1732f41c84f2756d053a53ae09c19
Rigo Rojas <rigo.rojas@nfl.com>
2017-01-30 16:35:43 -0800

    Refactor: merge style back to component

--- src/Scenes/Blank/Blank.js
+++ src/Scenes/Blank/Blank.js
@@ -1,9 +1,33 @@
 import React from 'React';
 import {StyleSheet, Image} from 'react-native';
 import {Actions} from 'react-native-router-flux';
-import styles from "./styles";
 import Button from 'react-native-button';
 const backgroundImage = require('../../Themes/default/images/bg_main.png');
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 60,
+        paddingTop: 20,
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: "stretch"
+    },
+    buttonContainer: {
+        padding: 10,
+        height: 45,
+        overflow: 'hidden',
+        borderRadius: 4,
+        backgroundColor: '#aaa',
+        marginBottom: 5,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    button: {
+        fontSize: 14,
+        color: '#fff'
+    },
+});
 
 export default class Blank extends React.Component {
     render() {
