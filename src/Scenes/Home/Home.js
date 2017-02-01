241e6a07a2f9884e8398925005e38e7b876341b9
Rigo Rojas <rigo.rojas@nfl.com>
2017-01-31 17:37:34 -0800

    Created the stand alone Profile Scene

--- src/Scenes/Home/Home.js
+++ src/Scenes/Home/Home.js
@@ -1,69 +1,12 @@
 import React from 'React';
 import {StyleSheet, Image, View, Text} from 'react-native';
-import Button from 'react-native-button';
 import {TabNavigator} from "react-navigation";
 import Notification from "./Notification.js";
-const backgroundImage = require('../../Themes/default/images/bg_main.png');
-
-const styles = StyleSheet.create({
-    container: {
-        marginTop: 60,
-        paddingTop: 20,
-        flex: 1,
-        width: null,
-        height: null,
-        resizeMode: "stretch"
-    },
-    buttonContainer: {
-        padding: 10,
-        height: 45,
-        overflow: 'hidden',
-        borderRadius: 4,
-        backgroundColor: '#aaa',
-        marginBottom: 5,
-        marginLeft: 20,
-        marginRight: 20,
-    },
-    button: {
-        fontSize: 14,
-        color: '#fff'
-    },
-});
-
-class HomeScreen extends React.Component {
-    render() {
-      return (
-        <Image
-            style={styles.container}
-            source={backgroundImage}
-        >
-            <Button
-                onPress={() => {
-                    Actions.refresh({
-                        key: 'Drawer',
-                        open: value => !value
-                    })
-                }}
-                containerStyle={styles.buttonContainer}
-                style={styles.button}
-            >
-                Go To Drawer
-            </Button>
-            <Button
-                onPress={() => Actions.Temperature()}
-                containerStyle={styles.buttonContainer}
-                style={styles.button}
-            >
-                Temperature Page
-            </Button>
-        </Image>
-      )
-    }
-};
+import Profile from "./Profile.js";
 
 const Home = TabNavigator(
     {
-        Home: {screen: HomeScreen},
+        Home: {screen: Profile},
         Notifications: {screen: Notification}
     },
     {
---
+++ src/Scenes/Home/Profile.js
@@ -0,0 +1,60 @@
+import React from 'React';
+import {StyleSheet, Image, View, Text} from 'react-native';
+import Button from 'react-native-button';
+const backgroundImage = require('../../Themes/default/images/bg_main.png');
+
+const styles = StyleSheet.create({
+    container: {
+        marginTop: 60,
+        paddingTop: 20,
+        flex: 1,
+        width: null,
+        height: null,
+        resizeMode: "stretch"
+    },
+    buttonContainer: {
+        padding: 10,
+        height: 45,
+        overflow: 'hidden',
+        borderRadius: 4,
+        backgroundColor: '#aaa',
+        marginBottom: 5,
+        marginLeft: 20,
+        marginRight: 20,
+    },
+    button: {
+        fontSize: 14,
+        color: '#fff'
+    },
+});
+
+export default class Profile extends React.Component {
+    render() {
+      return (
+        <Image
+            style={styles.container}
+            source={backgroundImage}
+        >
+            <Button
+                onPress={() => {
+                    Actions.refresh({
+                        key: 'Drawer',
+                        open: value => !value
+                    })
+                }}
+                containerStyle={styles.buttonContainer}
+                style={styles.button}
+            >
+                Go To Drawer
+            </Button>
+            <Button
+                onPress={() => Actions.Temperature()}
+                containerStyle={styles.buttonContainer}
+                style={styles.button}
+            >
+                Temperature Page
+            </Button>
+        </Image>
+      )
+    }
+};
