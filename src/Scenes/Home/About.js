56b321d16621b519db2e6980ab87692bf9bf7ec8
Rigo Rojas <rigo.rojas@nfl.com>
2017-01-31 17:49:52 -0800

    Created the about scene

---
+++ src/Scenes/Home/About.js
@@ -0,0 +1,12 @@
+import React from 'React';
+import {StyleSheet, Image, View, Text} from 'react-native';
+
+export default class About extends React.Component{
+    render() {
+        return (
+            <View>
+                <Text>This is the Stand alone About Scene</Text>
+            </View>
+        );
+    }
+}
