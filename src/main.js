93e10f78ffe2ce0d1a6e69d58e6e40d221827f89
Rigo Rojas <rigo.rojas@nfl.com>
2017-05-05 14:53:49 -0700

    platform agnostic ios/android

---
+++ src/main.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import {StyleSheet, Text, View, Image} from 'react-native';
+import Button from 'react-native-button';
+import {StackNavigator, TabNavigator, DrawerNavigator} from "react-navigation";
+import Menu from './SharedComponents/Drawer/Menu.js';
+import User from "./Scenes/User/Index.js";
+import Welcome from "./Scenes/Welcome/Index.js";
+import Theme from "./Themes/default/styles/styles.js";
+import RemoteImages from "./Scenes/RemoteImages/RemoteImages.js";
+
+const RightButtonImage = require('./Themes/default/images/gear.png');
+
+class DrawerMenuScreen extends React.Component {
+    render(){
+        return(
+            <Menu
+                theTitle="My New Title Prop"
+                {...this.props}
+            />
+        );
+    }
+}
+
+const Main = DrawerNavigator(
+    {
+        AppNavigator: {
+            screen: StackNavigator(
+                {
+                    Welcome:{
+                        name: 'Welcome',
+                        description: 'Just The Welcome Screen',
+                        screen: Welcome,
+                    },
+                    User: {
+                        name: 'User',
+                        description: 'User Tabs',
+                        screen: User,
+                    },
+                    RemoteImages: {
+                        name: 'RemoteImages',
+                        description: 'Remote Images',
+                        screen: RemoteImages,
+                     },
+                },
+                {
+                    initialRouteName: 'Welcome',
+                    headerMode: 'float',
+                },
+            )
+        },
+    },
+    {
+        contentComponent: DrawerMenuScreen,
+        drawerWidth: 300,
+        style: {},
+    }
+);
+
+export default Main;
