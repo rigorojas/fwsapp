d899b980e95d50fcd9b6f01a24a2016a27895e8e
Rigo Rojas <rigo.rojas@nfl.com>
2017-06-29 19:06:46 -0700

    Connected OpenWeatherAPI

--- src/Components/OpenWeatherMap/Api.js
+++ src/Components/OpenWeatherMap/Api.js
@@ -3,24 +3,23 @@
 const rootUrl = 'http://api.openweathermap.org/data/2.5/weather?APPID=30d4f5b2038a8b73139044f69026e30c';
 
 let kelvinToF = (kelvin) => {
-  return Math.round((kelvin - 273.15) * 1.8 +32) + ' ˚F';
+    return Math.round((kelvin - 273.15) * 1.8 +32) + ' ˚F';
 };
 
 let Api = (latitude, longitude) => {
-  const url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
-  return fetch(url)
+    const url = `${rootUrl}&lat=${latitude}&lon=${longitude}`;
+    return fetch(url)
     .then((response) => {
         return response.json();
     })
     .then((json) => {
-      return {
-        city: json.name,
-        temperature: kelvinToF(json.main.temp),
-        description: json.weather[0].description
-      }
-    }
-  )
-  .catch((err) => console.log("Api", err));
+        return {
+            city: json.name,
+            temperature: kelvinToF(json.main.temp),
+            description: json.weather[0].description
+        }
+    })
+    .catch((err) => console.log("Api", err));
 }
 
 export default Api;
--- src/Screens/ApiConnections/Index.js
+++ src/Screens/ApiConnections/Index.js
@@ -1,20 +1,71 @@
 import React from 'React';
 import {StyleSheet, Text, View, Image} from 'react-native';
+import OpenWeatherMapApi from "../../Components/OpenWeatherMap/Api.js";
+import MapView from 'react-native-maps';
 import Theme from "../../Themes/default/styles/styles.js";
 
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'stretch',
+    },
+    textWrapper: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    text: {
+        padding: 10,
+        fontSize: 20,
+        color: "#cecece",
+        backgroundColor: 'transparent',
+        fontWeight: "bold"
+    }
+});
+
+const region = {
+    name: "London",
+    latitude: 51.51,
+    longitude: -0.13
+};
+
 export default class ApiConnections extends React.Component {
+    state = {
+        city: '',
+        temperature: '',
+        description: '',
+     };
+
     static navigationOptions = {
-      title: 'Api Connections',
+        title: 'Api Connections',
+    };
+
+    componentDidMount(){
+        this.onRegionChangeComplete(region);
+    }
+
+    onRegionChangeComplete = (region) => {
+        OpenWeatherMapApi(
+            region.latitude,
+            region.longitude
+        )
+        .then((data) => {
+            this.setState(data);
+        });
     };
 
     render() {
         return(
-                <Image
-                    source={Theme.backgrounds.main}
-                    style={Theme.styles.sceneContainer}
-                >
-                    <Text>This is the API scene</Text>
-                </Image>
+            <Image
+                source={Theme.backgrounds.main}
+                style={Theme.styles.sceneContainer}
+            >
+                <View style={styles.textWrapper}>
+                    <Text style={styles.text}>{this.state.city}</Text>
+                    <Text style={styles.text}>{this.state.temperature}</Text>
+                    <Text style={styles.text} >{this.state.description}</Text>
+                </View>
+            </Image>
         );
     }
 }
