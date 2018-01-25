180ab85be59b904c49ae7ec9d879814848bf75aa
Rigo Rojas <rigo.rojas@nfl.com>
2018-01-24 19:00:22 -0800

    added redux-logger package

--- src/Store/Store.js
+++ src/Store/Store.js
@@ -1,13 +1,21 @@
 import {
+    applyMiddleware,
     combineReducers,
     createStore
 } from 'redux';
+import {createLogger} from 'redux-logger';
 import counterReducer from '../Screens/Counter/Reducer.js';
 
+const logger = createLogger();
+const initialState = {};
 
 // const reducers = combineReducers({
-//     counterReducer,
-//     dummyReducer,
+//     counterReducer
 // });
 
+// export default createStore(
+//     counterReducer,
+//     initialState,
+//     applyMiddleware(logger)
+// );
 export default createStore(counterReducer);
